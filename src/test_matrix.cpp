d2b5c08733261f2a60664f905913fa505b5f5ef9
Christian Herold <harryherold@googlemail.com>
2013-11-21 20:32:19 +0100

    implemented tests of add, sub and negation

--- src/test_matrix.cpp
+++ src/test_matrix.cpp
@@ -10,8 +10,8 @@ TEST_GROUP(Matrix)
 TEST(Matrix, Construction)
 {
   Matrix < int >iMatrix(20, 10);
-  LONGS_EQUAL(iMatrix.getHeight(), 10);
-  LONGS_EQUAL(iMatrix.getWidth(), 20);
+  LONGS_EQUAL(10,iMatrix.getHeight());
+  LONGS_EQUAL(20,iMatrix.getWidth());
 }
 
 TEST(Matrix, Fill)
@@ -24,18 +24,58 @@ TEST(Matrix, Fill)
 
   for (auto i = 0; i < 10; i++)
     for (auto j = 0; j < 10; j++)
-      LONGS_EQUAL(iMatrix(i, j), 42);
+      LONGS_EQUAL(42,iMatrix(i, j));
 }
 
-//~ TEST(Matrix, Eye)
-//~ {
-  //~ Matrix < int >m  = Matrix < int >::eye(10);
+TEST(Matrix, Eye)
+{
+  Matrix < int > m ( 10 );
+  m.eye();
+  for (auto i = 0; i < 10; i++) {
+    LONGS_EQUAL(1,m(i, i));
+  }
+}
 
-  //~ for (auto i = 0; i < 10; i++) {
-    //~ LONGS_EQUAL(m(i, i), 1);
-  //~ }
-//~ }
+TEST(Matrix, Add)
+{
+  Matrix < int > m1 ( 10 );
+  Matrix < int > m2 ( 10 );
+  Matrix < int > m3 ( 10 );
 
+  for (auto i = 0; i < 10; i++)
+  {
+    for (auto j = 0; j < 10; j++)
+    {
+      m1(i, j) = 42;
+      m2(i, j) = 42;
+    }
+  }
+  m3 = m2 + m1;
+
+  for (auto i = 0; i < 10; i++)
+    for (auto j = 0; j < 10; j++)
+      LONGS_EQUAL(84,m3(i, j));
+}
+
+TEST(Matrix, Sub)
+{
+  Matrix < int > m1 ( 10 );
+  Matrix < int > m2 ( 10 );
+  Matrix < int > m3 ( 10 );
+
+  for (auto i = 0; i < 10; i++)
+  {
+    for (auto j = 0; j < 10; j++)
+    {
+      m1(i, j) = 42;
+      m2(i, j) = 21;
+    }
+  }
+  m3 = m1 - m2;
+  for (auto i = 0; i < 10; i++)
+    for (auto j = 0; j < 10; j++)
+      LONGS_EQUAL(21,m3(i, j));
+}
 int
 main(int argc, char **argv)
 {
